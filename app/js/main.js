76683e6e493aec33b2351020cc06278bb47ea0b7
Nikittansk <61756114+Nikittansk@users.noreply.github.com>
2022-03-06 21:49:13 +0300

    finish

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,38 @@
-console.log('Init');
+let profile = document.querySelector('.profile');
+let burger = document.querySelector('.burger');
+let bgIndex = 0;
+let bgInterval = 10000;
+let bgElement = document.querySelector('.head');
+const bgImages = [
+    '../images/bg1.jpg',
+    '../images/bg2.jpg'
+];
+
+// Выпадающее меню
+if (document.documentElement.clientWidth > 920) {
+    profile.addEventListener('click', function() {
+        document.querySelector('.dropdown__wrapper').classList.toggle('hidden');
+    });
+}
+
+// Бургер
+burger.addEventListener('click', function() {
+    document.querySelector('.header__inner-wrapper').classList.toggle('active');
+});
+
+// Смена фона
+function bgChange() {
+    bgElement.style.backgroundImage = 'url(' + bgImages[bgIndex] + ')';
+    bgIndex++;
+    if (bgIndex >= bgImages.length) {
+        bgIndex = 0;
+    }
+}
+bgChange();
+window.setInterval(bgChange, bgInterval);
+
+
+
+
+
+
